50d227dbc7d1a6abc6716ffed9f0383ceda59c44
Chris Weed <chris@cjweed.com>
2024-07-10 15:05:59 -0500

    Refactored, now ready for attacks!

--- pirate.ts
+++ pirate.ts
@@ -3,7 +3,6 @@ type ActionObject = {
     parry: () => void
     faceLeft: () => void
     faceRight: () => void
-    goIdle: () => void
 }
 
 class Pirate {
@@ -25,8 +24,7 @@ class Pirate {
         attack: () => undefined,
         parry: () => undefined,
         faceLeft: () => undefined,
-        faceRight: () => undefined,
-        goIdle: () => undefined
+        faceRight: () => undefined
     }
 
     public health: number
@@ -37,6 +35,79 @@ class Pirate {
         this.facing = 'right'
 
         this.currentSprite = sprites.create(assets.image`Pirate`)
+        this._setupAnimations()
+        // Setup multiplayer
+        mp.setPlayerSprite(mp.getPlayerByNumber(playerNumber), this.currentSprite)
+
+        // Setup the controller handlers
+        this.controller = control
+        // We can't simply do `this.attack` as the event listeners can't handle a "lambda"
+        // And we need to keep a reference to the callback function so we can removeEventListner
+        this.action.attack = () => this.attack()
+        this.action.parry = () => this.parry()
+        this.action.faceLeft = () => this.face('left')
+        this.action.faceRight = () => this.face('right')
+
+        this.controller.A.addEventListener(ControllerButtonEvent.Pressed, this.action.attack)
+        // this.controller.B.addEventListener(ControllerButtonEvent.Pressed, this.action.parry)
+        this.controller.left.addEventListener(ControllerButtonEvent.Pressed, this.action.faceLeft)
+        this.controller.right.addEventListener(ControllerButtonEvent.Pressed, this.action.faceRight)
+    }
+
+    public place(x: number, y: number) {
+        // Starting location or teleport
+        this.currentSprite.x = x
+        this.currentSprite.y = y
+    }
+
+    public destroy() {
+        this.currentSprite.destroy()
+        // Remove all event listeners
+        this.controller.A.removeEventListener(ControllerButtonEvent.Pressed, this.action.attack)
+        this.controller.B.removeEventListener(ControllerButtonEvent.Pressed, this.action.parry)
+        this.controller.left.removeEventListener(ControllerButtonEvent.Pressed, this.action.faceLeft)
+        this.controller.right.removeEventListener(ControllerButtonEvent.Pressed, this.action.faceRight)
+    }
+
+    public render() {
+        this.currentSprite.x += this.controller.dx(50)
+        this.currentSprite.y += this.controller.dy(50)
+        this.currentSprite.z = this.currentSprite.y
+    }
+
+    parry() {
+        console.log('parry ' + this.controller.playerIndex)
+    }
+
+    attack() {
+        const attackAnimationSpeed = 50
+        if (this.facing === 'right') {
+            animation.runImageAnimation(
+                this.currentSprite,
+                Pirate.attackRightAnimation,
+                attackAnimationSpeed,
+                false
+            )
+        } else {
+            animation.runImageAnimation(
+                this.currentSprite,
+                Pirate.attackLeftAnimation,
+                attackAnimationSpeed,
+                false
+            )
+        }
+    }
+
+    face(direction: 'left' | 'right') {
+        console.log('Face ' + direction)
+        if (direction === 'left' && this.facing === 'right') {
+            this.facing = 'left'
+        } else if (direction === 'right' && this.facing === 'left') {
+            this.facing = 'right'
+        }
+    }
+
+    _setupAnimations() {
         // Setup animations
         // These numbers are coming from the source code: https://github.com/microsoft/arcade-character-animations/blob/main/main.ts
         characterAnimations.loopFrames(
@@ -95,100 +166,5 @@ class Pirate {
             // Moving down (facing right):
             8 + 256
         )
-
-        // this.idle()
-        // Setup multiplayer
-        mp.setPlayerSprite(mp.getPlayerByNumber(playerNumber), this.currentSprite)
-
-        // Setup the controller handlers
-        this.controller = control
-        // We can't simply do `this.attack` as the event listeners can't handle a "lambda"
-        // And we need to keep a reference to the callback function so we can removeEventListner
-        this.action.attack = () => this.attack()
-        this.action.parry = () => this.parry()
-        this.action.faceLeft = () => this.face('left')
-        this.action.faceRight = () => this.face('right')
-        this.action.goIdle = () => this.idle()
-
-        this.controller.A.addEventListener(ControllerButtonEvent.Pressed, this.action.attack)
-        // this.controller.B.addEventListener(ControllerButtonEvent.Pressed, this.action.parry)
-        this.controller.left.addEventListener(ControllerButtonEvent.Pressed, this.action.faceLeft)
-        this.controller.right.addEventListener(ControllerButtonEvent.Pressed, this.action.faceRight)
-        this.controller.left.addEventListener(ControllerButtonEvent.Released, this.action.goIdle)
-        this.controller.right.addEventListener(ControllerButtonEvent.Released, this.action.goIdle)
-    }
-
-    public place(x: number, y: number) {
-        // Starting location or teleport
-        this.currentSprite.x = x
-        this.currentSprite.y = y
-    }
-
-    public destroy() {
-        this.currentSprite.destroy()
-        // Remove all event listeners
-        this.controller.A.removeEventListener(ControllerButtonEvent.Pressed, this.action.attack)
-        this.controller.B.removeEventListener(ControllerButtonEvent.Pressed, this.action.parry)
-        this.controller.left.removeEventListener(ControllerButtonEvent.Pressed, this.action.faceLeft)
-        this.controller.right.removeEventListener(ControllerButtonEvent.Pressed, this.action.faceRight)
-        this.controller.left.removeEventListener(ControllerButtonEvent.Released, this.action.goIdle)
-        this.controller.right.removeEventListener(ControllerButtonEvent.Released, this.action.goIdle)
-    }
-
-    public render() {
-        this.currentSprite.x += this.controller.dx(50)
-        this.currentSprite.y += this.controller.dy(50)
-        this.currentSprite.z = this.currentSprite.y
-    }
-
-    parry() {
-        console.log('parry ' + this.controller.playerIndex)
-    }
-
-    attack() {
-        const attackAnimationSpeed = 50
-        if (this.facing === 'right') {
-            animation.runImageAnimation(
-                this.currentSprite,
-                Pirate.attackRightAnimation,
-                attackAnimationSpeed,
-                false
-            )
-        } else {
-            animation.runImageAnimation(
-                this.currentSprite,
-                Pirate.attackLeftAnimation,
-                attackAnimationSpeed,
-                false
-            )
-        }
-    }
-
-    face(direction: 'left' | 'right') {
-        console.log('Face ' + direction)
-        if (direction === 'left' && this.facing === 'right') {
-            this.facing = 'left'
-        } else if (direction === 'right' && this.facing === 'left') {
-            this.facing = 'right'
-        }
-    }
-
-    idle() {
-        console.log('Going idle ' + this.facing)
-        // if (this.facing === 'left') {
-        //     animation.runImageAnimation(
-        //         this.currentSprite,
-        //         Pirate.idleLeftAnimation,
-        //         200,
-        //         true
-        //     )
-        // } else {
-        //     animation.runImageAnimation(
-        //         this.currentSprite,
-        //         Pirate.idleRightAnimation,
-        //         200,
-        //         true
-        //     )
-        // }
     }
 }
