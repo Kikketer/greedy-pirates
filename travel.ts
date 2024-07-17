d4e84d085b78e5d961994cbc2ca3710bb297ff0c
Chris Weed <chris@cjweed.com>
2024-07-17 17:34:45 -0500

    Animations for travel

--- travel.ts
+++ travel.ts
@@ -1,10 +1,11 @@
 namespace Travel {
     let _onBoatBattle: () => void
     let _onLandOnIsland: () => void
+    let _videoSprite: Sprite
     let _textSprite: Sprite
-    let _startAnimation: Image[]
-    let _pirateAnimation: Image[]
-    let _islandAnimation: Image[]
+    const _startAnimation: Image[] = assets.animation`Boat Float`
+    const _pirateAnimation: Image[] = assets.animation`Boat Float Enemy Spotted`
+    const _islandAnimation: Image[] = assets.animation`Boat Float Island`
 
     export function init({ targetIsland }: { targetIsland: Map.Island }) {
         // Probably should have done this better... oh well
@@ -19,29 +20,53 @@ namespace Travel {
         scene.setBackgroundColor(0)
         scene.setBackgroundImage(assets.image`empty`)
 
+        _videoSprite = sprites.create(image.create(80, 60))
+        _videoSprite.x = 80
+        _videoSprite.y = 60
+
+        animation.runImageAnimation(
+            _videoSprite,
+            _startAnimation,
+            500,
+            true
+        )
+
         // Animate!
         _textSprite = textsprite.create('Floaty boaty', 0, 14)
         _textSprite.x = 80
-        _textSprite.y = 60
+        _textSprite.y = 100
+        pause(2000)
 
         if (result > oddsOfBoatBattle) {
-            pause(3000)
+            animation.runImageAnimation(
+                _videoSprite,
+                _pirateAnimation,
+                500,
+                false
+            )
+
             _textSprite.destroy()
             _textSprite = textsprite.create('Thar be pirates!')
             _textSprite.x = 80
-            _textSprite.y = 60
-            pause(2000)
+            _textSprite.y = 100
+            pause(_pirateAnimation.length * 500 + 1000)
             
             destroy()
             _onBoatBattle()
         } else {
-            pause(3000)
+            animation.runImageAnimation(
+                _videoSprite,
+                _islandAnimation,
+                500,
+                false
+            )
+
             _textSprite.destroy()
             const text = targetIsland.id === 0 ? 'Ye hid yer booty!' : 'Arrgh, steal thee loot!'
             _textSprite = textsprite.create(text)
             _textSprite.x = 80
-            _textSprite.y = 60
-            pause(2000)
+            _textSprite.y = 100
+            pause(_islandAnimation.length * 500 + 1000)
 
             destroy()
             _onLandOnIsland()
@@ -51,6 +76,9 @@ namespace Travel {
     function destroy() {
         if (_textSprite) {
             _textSprite.destroy()
+        }
+        if (_videoSprite) {
+            _videoSprite.destroy()
         }
         scene.setBackgroundImage(assets.image`empty`)
         scene.setBackgroundColor(0)
